acc0dc4c8908e95a2b52040d519b7ea0a1d3703b
Kebai <2804673379@qq.com>
2024-04-30 19:14:20 +0800

    chore(web): add graphql header

--- packages/web/src/layout/protected-layout/index.tsx
+++ packages/web/src/layout/protected-layout/index.tsx
@@ -1,18 +1,24 @@
-import React from "react"
-import { Outlet, useLoaderData } from "react-router-dom"
-import { protectedLoader } from "./loader"
-import { initUserStore } from "@/store/user"
+import React from 'react';
+import { Outlet, useLoaderData } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { GET_ORGANIZATION } from '@/services/graphql';
+import { initUserStore } from '@/store/user';
+import { protectedLoader } from './loader';
 
 const ProtectedLayout: React.FC = () => {
-  const user = useLoaderData() as KB.UserInfo
+  const user = useLoaderData() as KB.UserInfo;
+  const { loading, data } = useQuery(GET_ORGANIZATION);
 
-  initUserStore(user)
+  initUserStore(user);
+
+  console.log(loading, data);
 
   return (
     <div className="h-screen">
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Object.assign(ProtectedLayout, { loader: protectedLoader })
+export default Object.assign(ProtectedLayout, { loader: protectedLoader });
--- packages/web/src/services/graphql.ts
+++ packages/web/src/services/graphql.ts
@@ -1,6 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@/__generated__/gql';
+import { USER_TOKEN } from '@/lib/constants';
 
 export const graphqlClient = new ApolloClient({
   uri: `${import.meta.env.VITE_STRAPI_URL}/graphql`,
   cache: new InMemoryCache(),
-})
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem(USER_TOKEN)}`,
+  },
+});
+
+export const GET_ORGANIZATION = gql(/* GraphQL */ `
+  query Organization {
+    organization {
+      data {
+        id
+        attributes {
+          host
+          name
+        }
+      }
+    }
+  }
+`);
